3165bf2d48f871b48d9479f9714afc2a1a575bec
mustafa-ali-qbatch <mustafa.ali@qbatch.com>
2023-07-18 16:42:27 +0500

    first commit

--- src/app/Todos/page.js
+++ src/app/Todos/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Navbar from "./navbar"
-export default function Home() {
+import Navbar from "../navbar"
+export default function Todo() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar/>
-      <p>Home Page</p>
+      <p>todo Page</p>
      <div>Integrating Material UI In Next JS APP.</div>
     </main>
   )
