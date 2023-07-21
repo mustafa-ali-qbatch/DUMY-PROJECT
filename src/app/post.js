57c4b109364794b42f18345770accb805c63dd01
mustafa-ali-qbatch <mustafa.ali@qbatch.com>
2023-07-21 20:36:49 +0500

    Chnages in post

--- src/app/post.js
+++ src/app/post.js
@@ -1,39 +1,60 @@
-export default function Post({data}) {
-    return (
-      <>
-        <div className="p-8 bg-gray-50 dark:bg-gray-900 flex ">
-          <div className="px-5 py-4 bg-white dark:bg-gray-800 shadow rounded-lg max-width">
-            <div className="flex mb-4">
-              <img
-                className="w-12 h-12 rounded-full"
-                src={data.image}
-                alt="Profile"
-              />
-              <div className="ml-2 mt-0.5">
-                <span className="block font-medium text-base leading-snug text-black dark:text-gray-100">
-                  {data.author}
-                </span>
-                <span className="block text-sm text-gray-500 dark:text-gray-400 font-light leading-snug">
-                  {data.timestamp}
-                </span>
-              </div>
+export default function Post({ data }) {
+  return (
+    <>
+      <div className="p-8 bg-gray-50 dark:bg-gray-900 flex ">
+        <div className="px-5 py-4 bg-white dark:bg-gray-800 shadow rounded-lg max-width">
+          <div className="flex mb-4">
+            <img
+              className="w-12 h-12 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="Profile"
+            />
+            <div className="ml-2 mt-0.5">
+              <span className="block font-medium text-base leading-snug text-black dark:text-gray-100">
+                {data.author}
+              </span>
+              <span className="block text-sm text-gray-500 dark:text-gray-400 font-light leading-snug">
+                {data.timestamp}
+              </span>
             </div>
-            <p className="text-gray-800 dark:text-gray-100 leading-snug md:leading-normal">
-              {data.content}
-            </p>
-            <div className="flex justify-between items-center mt-5">
-              <div className="flex ">
-                <span className="ml-1 text-gray-500 dark:text-gray-400 font-light">
-                  {data.likes}
-                </span>
-              </div>
-              <div className="ml-1 text-gray-500 dark:text-gray-400 font-light">
-                {data.comments} comments
-              </div>
+          </div>
+          <p className="text-gray-800 dark:text-gray-100 leading-snug md:leading-normal">
+            {data.content}
+          </p>
+          <div className="flex items-center justify-between pb-2 border-b border-gray-300 text-gray-500 text-sm"></div>
+          <div className="flex justify-between items-center mt-5">
+            <div className="flex ">
+              <span className="ml-1 text-gray-500 dark:text-gray-400 font-light">
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    <button className="focus:outline-none flex items-center justify-center w-4 h-4 rounded-full bg-red-500 text-white">
+                      <i style={{ fontSize: 10 }} className="fas fa-heart"></i>
+                    </button>
+                    <button className="focus:outline-none flex items-center justify-center w-4 h-4 rounded-full bg-primary text-white">
+                      <i
+                        style={{ fontSize: 10 }}
+                        className="fas fa-thumbs-up"
+                      ></i>
+                    </button>
+                    <button className="focus:outline-none flex items-center justify-center w-4 h-4 rounded-full bg-yellow-500 text-white">
+                      <i
+                        style={{ fontSize: 10 }}
+                        className="fas fa-surprise"
+                      ></i>
+                    </button>
+                    <div className="ml-1">
+                      <p>{data.likes}</p>
+                    </div>
+                  </div>
+                </div>
+              </span>
+            </div>
+            <div className="ml-1 text-gray-500 dark:text-gray-400 font-light">
+              {data.comments} comments
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </>
+  );
+}
